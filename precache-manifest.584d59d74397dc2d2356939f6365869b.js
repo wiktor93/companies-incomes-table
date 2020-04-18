6ef860d20e35d6c8f2df18ce3efbb7d06eccc1f9
Wiktor Walczak <w.walczak93@gmail.com>
2020-04-18 10:48:52 +0200

    Updates

---
+++ precache-manifest.584d59d74397dc2d2356939f6365869b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db0532dbfc99d3425f463619951e2e6d",
+    "url": "/companies-incomes-table/index.html"
+  },
+  {
+    "revision": "d772b1132cad7a99ac25",
+    "url": "/companies-incomes-table/static/css/main.ad83d409.chunk.css"
+  },
+  {
+    "revision": "ecfb2cd713a49b7d9f11",
+    "url": "/companies-incomes-table/static/js/2.cf381f6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/companies-incomes-table/static/js/2.cf381f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d772b1132cad7a99ac25",
+    "url": "/companies-incomes-table/static/js/main.6f6fc9f4.chunk.js"
+  },
+  {
+    "revision": "5f6d47a5b4da8fe9499c",
+    "url": "/companies-incomes-table/static/js/runtime-main.33a019ca.js"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/companies-incomes-table/static/media/left-arrow.6c9efe0c.svg"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/companies-incomes-table/static/media/right-arrow.be237328.svg"
+  },
+  {
+    "revision": "ca82bc743710e8f84904d035d92bf5cd",
+    "url": "/companies-incomes-table/static/media/sort-down.ca82bc74.svg"
+  },
+  {
+    "revision": "3a5f2344c363f163317b4c063497fac9",
+    "url": "/companies-incomes-table/static/media/sort-up.3a5f2344.svg"
+  }
+]);
